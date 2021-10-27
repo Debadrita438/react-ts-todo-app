b727534903e051fbd0976c7ad557b690c587883c
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-27 11:40:09 +0530

    Adding a todo form

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+    const todoInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        // const enteredText = todoInputRef.current?.value // value property will only exists if current property is there, otherwise it's undefined
+        const enteredText = todoInputRef.current!.value; // value is definitely there and type of it is string
+
+        if (enteredText.trim().length === 0) {
+            return;
+        }
+
+        props.onAddTodo(enteredText);
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor='todo'>Todo Text</label>
+            <input type='text' id='todo' ref={todoInputRef} />
+            <button>Add Todo</button>
+        </form>
+    );
+};
+
+export default NewTodo;
